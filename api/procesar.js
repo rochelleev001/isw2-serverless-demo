5c88c71a28550ab8796156092adb9b871045ceb1
rochelleev001 <115517564+rochelleev001@users.noreply.github.com>
2026-02-09 18:51:37 -0600

    Restaura el comportamiento esperado

--- api/procesar.js
+++ api/procesar.js
@@ -1,8 +1,7 @@
 export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
   
-  // SIMPLIFICADO para el taller básico
   res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toLowerCase()}`
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
   });
 }
